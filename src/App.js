289f9b11c4eed55f863d84d55641ed6d04854ce8
TaniaLizz-03 <71456511+TaniaLizz-03@users.noreply.github.com>
2020-11-16 15:53:15 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Parrafo from './components/Parrafo'
+import Contador from './components/Contador'
+
+function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <Parrafo></Parrafo>
+      <Parrafo></Parrafo>
+      <hr>
+      <Contador></Contador></hr>
+    </div>
+  );
+}
+
+export default App;
